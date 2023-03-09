13baeb31e97f3add19b3bb6a3f7f6488770fc501
Gonzalo <zapozapico@gmail.com>
2023-03-09 13:47:48 -0300

    fast refresh test

--- FrontNextjsReact/todo/next.config.js
+++ FrontNextjsReact/todo/next.config.js
@@ -1,4 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   output: 'standalone',
+// }
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-  output: 'standalone',
-}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, context) => {
+    // Enable polling based on env variable being set
+    if (process.env.NEXT_WEBPACK_USEPOLLING) {
+      config.watchOptions = {
+        poll: 500,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
